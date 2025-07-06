0862821ec635f6cb02ae4b3f80b57250e1c10b38
arpan jana <arpangroup1@gmail.com>
2025-07-07 03:27:32 +0530

    develop

--- src/pages/transaction/Transactions.jsx
+++ src/pages/transaction/Transactions.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import PageTitle from "../../components/page_title/PageTitle";
 import Badge from "../../components/Badge";
 
@@ -7,10 +7,33 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css"; // Add your preferred theme
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 import { NavLink } from 'react-router-dom';
+import { usePaginatedFetch } from '../../hooks/usePaginatedFetch';
 ModuleRegistry.registerModules([AllCommunityModule]);
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+
+  // Format: "Jul 06 2025 08:03"
+  const options = {
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  };
+
+  const formatted = date.toLocaleString('en-US', options);
+  // Reorder the parts to match: "Jul 06 2025 08:03"
+  const [monthDay, year, time] = formatted.split(', ');
+  return `${monthDay} ${year} ${time}`;
+}
 
 
 const Transactions = () => {
+  const pageSize = 9999;
+  const [page, setPage] = useState(0);
+  const { data, totalPages, loading } = usePaginatedFetch(`/api/v1/transactions`, page, pageSize, null);
 
   const UserCell = ({ data }) => {
     const { userId, user } = data;
@@ -21,48 +44,63 @@ const Transactions = () => {
     )
   }
 
-  const AmountCell = ({ value }) => {
+  const DateCell = ({ value }) => {
     if (!value) return null;
+    return <span>{formatDate(value)}</span>;
+  };
 
-    // Remove currency and commas, just check for minus sign
-    const isNegative = value.trim().startsWith('-');
+  const AmountCell = ({ value }) => {
+    if (value === null || value === undefined) return null;
+
+    const strValue = String(value); // Convert number to string
+    const isNegative = strValue.trim().startsWith('-');
 
     const style = {
       color: isNegative ? '#ef476f' : '#2a9d8f', // red or green
       fontWeight: 'bold',
     };
 
-    return <span style={style}>{value}</span>;
+    return <span style={style}>{strValue}{" INR"}</span>;
   };
 
-  const [rowData] = useState([
-    { date: "Jun 07 2025 05:26", userId: 1, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Withdraw", amount: "-2000 INR", gateway: "WITHDRAWAL", status: "Pending" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", txnId: "TRXPOZ5ZZYDTJ", type: "Investment", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 3, user: "ElonMusk9897", txnId: "TRXPOZ5ZZYDTJ", type: "Refund", amount: "5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 4, user: "SteveJobs5454", txnId: "TRXPOZ5ZZYDTJ", type: "Subtract", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 5, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Withdraw", amount: "-5000 INR", gateway: "WITHDRAWAL", status: "Cancelled" },
-    { date: "Jun 07 2025 05:26", userId: 6, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Investment", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 7, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Signup Bonus", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 8, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Deposit", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 9, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Refund", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 10, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Referral", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 11, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Exchange", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 12, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Manual Deposit", amount: "100 INR", gateway: "Cytugi", status: "Cancelled" },
-    { date: "Jun 07 2025 05:26", userId: 13, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Interest", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 14, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Bonus", amount: "-5000 INR", gateway: "System", status: "Success" },
-    { date: "Jun 07 2025 05:26", userId: 15, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Manual Deposit", amount: "100 INR", gateway: "Testbep201234567890", status: "Cancelled" },
-  ]);
+  // const [rowData] = useState([
+  //   { date: "Jun 07 2025 05:26", userId: 1, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Withdraw", amount: "-2000 INR", gateway: "WITHDRAWAL", status: "Pending" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", txnId: "TRXPOZ5ZZYDTJ", type: "Investment", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 3, user: "ElonMusk9897", txnId: "TRXPOZ5ZZYDTJ", type: "Refund", amount: "5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 4, user: "SteveJobs5454", txnId: "TRXPOZ5ZZYDTJ", type: "Subtract", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 5, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Withdraw", amount: "-5000 INR", gateway: "WITHDRAWAL", status: "Cancelled" },
+  //   { date: "Jun 07 2025 05:26", userId: 6, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Investment", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 7, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Signup Bonus", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 8, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Deposit", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 9, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Refund", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 10, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Referral", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 11, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Exchange", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 12, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Manual Deposit", amount: "100 INR", gateway: "Cytugi", status: "Cancelled" },
+  //   { date: "Jun 07 2025 05:26", userId: 13, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Interest", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 14, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Bonus", amount: "-5000 INR", gateway: "System", status: "Success" },
+  //   { date: "Jun 07 2025 05:26", userId: 15, user: "TestTest2362", txnId: "TRXPOZ5ZZYDTJ", type: "Manual Deposit", amount: "100 INR", gateway: "Testbep201234567890", status: "Cancelled" },
+  // ]);
 
   const [colDefs] = useState([
-    { field: "date", width: 150 },
-    { field: "user", width: 200 , cellRenderer: UserCell},
-    { field: "txnId", width: 150 },
-    { field: "type", width: 150, cellRenderer: (params) => <Badge value={params.value} style={{ background: '#5e3fc9' }} /> },
-    { field: "amount", width: 120, cellRenderer: AmountCell },
+    { field: "txnDate", headerName: 'DATE', width: 150, cellRenderer: DateCell },
+    { field: "user", headerName: 'USER', width: 200, cellRenderer: UserCell },
+    { field: "txnRefId", headerName: 'TXN_ID', width: 150 },
+    { field: "txnType", headerName: 'TYPE', width: 150, cellRenderer: (params) => <Badge value={params.value} style={{ background: '#5e3fc9' }} /> },
+    { field: "amount", headerName: 'AMOUNT', width: 120, cellRenderer: AmountCell },
     { field: "gateway", width: 150 },
     { field: "status", width: 120, cellRenderer: Badge },
   ]);
 
+  const defaultColDef = {
+    // flex: 1,    
+    minWidth: 80,
+    resizable: true,
+  };
+
+  const onPaginationChanged = useCallback((params) => {
+    const newPage = params.api.paginationGetCurrentPage();
+    setPage(newPage);
+  }, []);
 
 
 
@@ -79,9 +117,14 @@ const Transactions = () => {
                   <div style={{ height: 500 }} className="ag-theme-alpine">
                     <AgGridReact
                       theme={"legacy"}
-                      rowData={rowData}
+                      rowData={data}
+                      loading={loading}
                       columnDefs={colDefs}
-                      pagination={true} />
+                      defaultColDef={defaultColDef}
+                      pagination={true}
+                      paginationPageSize={10}
+                      paginationPageSizeSelector={[10, 20, 50, 100]}
+                      onPaginationChanged={onPaginationChanged} />
                   </div>
                 </div>
               </div>
--- src/pages/users/UserProfit.jsx
+++ src/pages/users/UserProfit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import PageTitle from "../../components/page_title/PageTitle";
 import Badge from "../../components/Badge";
 
@@ -7,10 +7,33 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css"; // Add your preferred theme
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 import { NavLink } from 'react-router-dom';
+import { usePaginatedFetch } from '../../hooks/usePaginatedFetch';
 ModuleRegistry.registerModules([AllCommunityModule]);
 
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+
+  // Format: "Jul 06 2025 08:03"
+  const options = {
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  };
+
+  const formatted = date.toLocaleString('en-US', options);
+  // Reorder the parts to match: "Jul 06 2025 08:03"
+  const [monthDay, year, time] = formatted.split(', ');
+  return `${monthDay} ${year} ${time}`;
+}
 
 const UserProfit = () => {
+  const pageSize = 9999;
+  const [page, setPage] = useState(0);
+  const { data, totalPages, loading } = usePaginatedFetch(`/api/v1/profits`, page, pageSize, null);
+
 
   const UserCell = ({ data }) => {
     const { userId, user } = data;
@@ -21,41 +44,61 @@ const UserProfit = () => {
     )
   }
 
-  const AmountCell = ({ value }) => {
+  const DateCell = ({ value }) => {
     if (!value) return null;
+    return <span>{formatDate(value)}</span>;
+  };
 
-    // Remove currency and commas, just check for minus sign
-    const isNegative = value.trim().startsWith('-');
+  const AmountCell = ({ value }) => {
+    if (value === null || value === undefined) return null;
+
+    const strValue = String(value); // Convert number to string
+    const isNegative = strValue.trim().startsWith('-');
 
     const style = {
-      color: isNegative ? '#ef476f' : '#06d6a0', // red or green
+      color: isNegative ? '#ef476f' : '#2a9d8f', // red or green
       fontWeight: 'bold',
     };
 
-    return <span style={style}>{value}</span>;
+    return <span style={style}>{strValue}{" INR"}</span>;
   };
 
-  const [rowData] = useState([
-    { date: "Jun 07 2025 05:26", userId: 1, user: "TestTest2362", type: "Signup Bonus", amount: "-+8 INR", profitFrom: "System", description: "Signup Bonus" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Signup Bonus", amount: "+8 INR", profitFrom: "System", description: "Signup Bonus" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Referral", amount: "+50 INR", profitFrom: "test2test27066", description: "Deposit Referral Bonus Via Test2 Test2 - Level 1" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Referral", amount: "+100 INR", profitFrom: "test2test27066", description: "Deposit Referral Bonus Via Test2 Test2 - Level 2" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Referral", amount: "+100 INR", profitFrom: "test2test27066", description: "Deposit Referral Bonus Via Test2 Test2 - Level 1" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Referral", amount: "+250 INR", profitFrom: "MinalPal6996 ", description: "Deposit Referral Bonus Via Minal Pal - Level 1" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Interest", amount: "+1 INR", profitFrom: "System ", description: "Crypto investment Plan Interest" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Interest", amount: "+1 INR", profitFrom: "System ", description: "Crypto investment Plan Interest" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Bonus", amount: "+10 INR", profitFrom: "System ", description: "Referral Bonus by Level 2" },
-    { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Bonus", amount: "+20 INR", profitFrom: "System ", description: "Referral Bonus by Level 2" },
-  ]);
+  // const [rowData] = useState([
+  //   { date: "Jun 07 2025 05:26", userId: 1, user: "TestTest2362", type: "Signup Bonus", amount: "-+8 INR", profitFrom: "System", description: "Signup Bonus" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Signup Bonus", amount: "+8 INR", profitFrom: "System", description: "Signup Bonus" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Referral", amount: "+50 INR", profitFrom: "test2test27066", description: "Deposit Referral Bonus Via Test2 Test2 - Level 1" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Referral", amount: "+100 INR", profitFrom: "test2test27066", description: "Deposit Referral Bonus Via Test2 Test2 - Level 2" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Referral", amount: "+100 INR", profitFrom: "test2test27066", description: "Deposit Referral Bonus Via Test2 Test2 - Level 1" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Referral", amount: "+250 INR", profitFrom: "MinalPal6996 ", description: "Deposit Referral Bonus Via Minal Pal - Level 1" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Interest", amount: "+1 INR", profitFrom: "System ", description: "Crypto investment Plan Interest" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Interest", amount: "+1 INR", profitFrom: "System ", description: "Crypto investment Plan Interest" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Bonus", amount: "+10 INR", profitFrom: "System ", description: "Referral Bonus by Level 2" },
+  //   { date: "Jun 07 2025 05:26", userId: 2, user: "JohnDoe123", type: "Bonus", amount: "+20 INR", profitFrom: "System ", description: "Referral Bonus by Level 2" },
+  // ]);
+
+
 
   const [colDefs] = useState([
-    { field: "date", width: 150 },
-    { field: "user", width: 150 , cellRenderer: UserCell},
-    { field: "amount", width: 120, cellRenderer: AmountCell },
-    { field: "type", width: 150, cellRenderer: (params) => <Badge value={params.value} style={{ background: '#5e3fc9' }} /> },
-    { field: "profitFrom", width: 150 },
-    { field: "description", width: 300 },
+    { field: "txnDate", headerName: 'DATE', width: 150, cellRenderer: DateCell },
+    { field: "user", headerName: 'USER', width: 200, cellRenderer: UserCell },
+    { field: "txnRefId", headerName: 'TXN_ID', width: 150 },
+    { field: "txnType", headerName: 'TYPE', width: 150, cellRenderer: (params) => <Badge value={params.value} style={{ background: '#5e3fc9' }} /> },
+    { field: "amount", headerName: 'AMOUNT', width: 120, cellRenderer: AmountCell },
+    { field: "gateway", width: 150 },
+    { field: "status", width: 120, cellRenderer: Badge },
   ]);
+
+  const defaultColDef = {
+    // flex: 1,    
+    minWidth: 80,
+    resizable: true,
+  };
+
+
+  const onPaginationChanged = useCallback((params) => {
+    const newPage = params.api.paginationGetCurrentPage();
+    setPage(newPage);
+  }, []);
 
 
 
@@ -73,9 +116,14 @@ const UserProfit = () => {
                   <div style={{ height: 500 }} className="ag-theme-alpine">
                     <AgGridReact
                       theme={"legacy"}
-                      rowData={rowData}
+                      rowData={data}
+                      loading={loading}
                       columnDefs={colDefs}
-                      pagination={true} />
+                      defaultColDef={defaultColDef}
+                      pagination={true}
+                      paginationPageSize={10}
+                      paginationPageSizeSelector={[10, 20, 50, 100]}
+                      onPaginationChanged={onPaginationChanged} />
                   </div>
                 </div>
               </div>
