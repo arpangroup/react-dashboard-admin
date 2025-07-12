a566e345841068e3dd7c602fb6f92e8d3892e29b
arpan jana <arpangroup1@gmail.com>
2025-07-13 03:45:32 +0530

    fix

--- src/constants/apiRoutes.js
+++ src/constants/apiRoutes.js
@@ -8,17 +8,9 @@ export const API_ROUTES = {
   SCHEMA_LIST: "/api/v1/investment-schemas",
   SCHEMA_By_ID: (id) =>`/api/v1/investment-schemas/${id}`,
   // Notification Templates
+  MAIL_CONNECTION_TEST: "/api/v1/notifications/mail-connection-test",
   TEMPLATE_LIST: (type) => `/api/v1/templates/${type}`,
   TEMPLATE_BY_ID: (type, id) => `/api/v1/templates/${type}/${id}`,
-
-
-  MAIL_CONNECTION_TEST: "/api/v1/notifications/mail-connection-test",
-  EMAIL_TEMPLATES: "/api/v1/templates/email",
-  SMS_TEMPLATES: "/api/v1/templates/sms",
-  PUSH_TEMPLATES: "/api/v1/templates/push",
-  EMAIL_TEMPLATES_BY_ID: (id) => `/api/v1/templates/email/${id}`,
-  SMS_TEMPLATES_BY_ID: (id) => `/api/v1/templates/sms/${id}`,
-  PUSH_TEMPLATES_BY_ID: (id) => `/api/v1/templates/push/${id}`,
   // Configs...
   CONFIG_PROPERTIES: "http://localhost:8888/nft_app/dev",
   UPDATE_CONFIG: "http://localhost:8888/api/v1/configs/update",
